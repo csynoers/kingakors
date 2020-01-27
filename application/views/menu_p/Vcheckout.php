09e16a3401b798d787e942e29c69bf04b79a2bd4
csynoers <3s0c9m7@gmail.com>
2020-01-28 04:10:30 +0700

    tes table fix

--- application/views/menu_p/Vcheckout.php
+++ application/views/menu_p/Vcheckout.php
@@ -39,27 +39,24 @@
                                 <table class="table table-striped">
                                   <thead>
                                     <tr>
-                                      <th>Firstname</th>
-                                      <th>Lastname</th>
-                                      <th>Email</th>
+                                      <th>Name</th>
+                                      <th>Price</th>
+                                      <th>Quantity</th>
                                     </tr>
                                   </thead>
                                   <tbody>
-                                    <tr>
-                                      <td>John</td>
-                                      <td>Doe</td>
-                                      <td>john@example.com</td>
-                                    </tr>
-                                    <tr>
-                                      <td>Mary</td>
-                                      <td>Moe</td>
-                                      <td>mary@example.com</td>
-                                    </tr>
-                                    <tr>
-                                      <td>July</td>
-                                      <td>Dooley</td>
-                                      <td>july@example.com</td>
-                                    </tr>
+                                    <?php
+                                      foreach ($keranjang as $key => $value) {
+                                        $value->hargaText = 'Rp. '.number_format($value->harga);
+                                        echo "
+                                          <tr>
+                                            <td>{$value->merek}</td>
+                                            <td>{$value->hargaText}</td>
+                                            <td>{$value->jumlah_pesan}</td>
+                                          </tr>
+                                        ";
+                                      }
+                                    ?>
                                   </tbody>
                                 </table>
                             <select class="w-100 d-none" id="id_met_pem" >
