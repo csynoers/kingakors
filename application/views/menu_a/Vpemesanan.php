14cc2b77e00633e9e762e02d7661c7b765c6f5a8
csynoers <3s0c9m7@gmail.com>
2020-01-30 21:11:05 +0700

    tes tabel >> 2

--- application/views/menu_a/Vpemesanan.php
+++ application/views/menu_a/Vpemesanan.php
@@ -14,10 +14,16 @@
                 <th>Firstname</th>
                 <th>Lastname</th>
                 <th>Email</th>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
             </tr>
         </thead>
         <tbody>
             <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
                 <td>John</td>
                 <td>Doe</td>
                 <td>john@example.com</td>
@@ -26,8 +32,14 @@
                 <td>Mary</td>
                 <td>Moe</td>
                 <td>mary@example.com</td>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
             </tr>
             <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
                 <td>July</td>
                 <td>Dooley</td>
                 <td>july@example.com</td>
