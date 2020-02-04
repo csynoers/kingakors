fabaab65d4f37054e46c204ec83e57f17c4d4e15
csynoers <3s0c9m7@gmail.com>
2020-02-04 16:21:51 +0700

    tes detail pembayaran >> fix metode pembayaran

--- application/controllers/Ctm/Criw.php
+++ application/controllers/Ctm/Criw.php
@@ -122,7 +122,9 @@ date_default_timezone_set('Asia/jakarta');
             if ( $key->verifikasi=='diproses' ) {
                 $metodePembayaran = $this->get_from_invoice( $this->getInvoice($key->external_id) );
             }
-            $metodePembayaran= $key->invoice_url;
+            if ( $key->verifikasi=='belum bayar' ) {
+                $metodePembayaran= '<a href="'.$key->invoice_url.'" target="_blank" class="btn-outline-primary border p-2">Bayar Sekarang</a>';
+            }
             // print_r($key);
             $out .= '<tr>
                 <td>' . $key->nama_pel . '</td>
