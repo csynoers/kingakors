d1bcb91696aecd093026689913d3f46338808aa5
csynoers <3s0c9m7@gmail.com>
2020-02-07 22:33:52 +0700

    modal to footer

--- application/views/template/Footer.php
+++ application/views/template/Footer.php
@@ -52,7 +52,24 @@
 
 
 </footer>
-
+<div class="modal fade bd-example-modal-xl" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal Title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Modal Body ...</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- ##### jQuery (Necessary for All JavaScript Plugins) ##### -->
 <!-- <script src="<?= base_url("assets/amado/js/jquery/jquery-2.2.4.min.js") ?>"></script> -->
 
