abe7bdc9b70357fe3bad9f93f2f16ec8c1c60011
csynoers <3s0c9m7@gmail.com>
2020-02-07 22:40:59 +0700

    remove modal >> 2

--- application/views/menu_a/Vdetailpemesanan.php
+++ application/views/menu_a/Vdetailpemesanan.php
@@ -1,1 +1,19 @@
-<?php echo $out ?>
+<div class="cart-table-area section-padding-50">
+    <div class="">
+        <div class="">
+
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div class="cart-title mt-50">
+                        <h2>Pemesanan</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <?php echo $out ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
