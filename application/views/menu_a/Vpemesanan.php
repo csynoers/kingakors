366e533828b3260ed2bcb7a24bf90a256691aa80
csynoers <3s0c9m7@gmail.com>
2020-02-07 22:13:28 +0700

    add kolom aksi, detail pesanan dan update status >> cek href >> add modal

--- application/views/menu_a/Vpemesanan.php
+++ application/views/menu_a/Vpemesanan.php
@@ -57,13 +57,39 @@
 
     <!-- ##### Main Content Wrapper End ##### -->
 
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
 <script>
     (function(j){
         j(document).on('click','.detail-pesanan',function(e){
-            
+            // Show the Modal on load
+            $("#myModal").modal("show");
         });
         j(document).on('click','.update-pesanan',function(e){
-            
+            // Show the Modal on load
+            $("#myModal").modal("show");
         });
     })(jQuery)
 </script>
