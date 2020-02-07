3c1832b970985cec23c462ac7f403e27d4d8b1a0
csynoers <3s0c9m7@gmail.com>
2020-02-07 21:32:46 +0700

    add kolom aksi, detail pesanan dan update status >> add class >> 1

--- application/views/menu_a/Vpemesanan.php
+++ application/views/menu_a/Vpemesanan.php
@@ -42,8 +42,8 @@
                                             <h5><?= $data_pemesanan->status_pesanan ?></h5>
                                         </td>
                                         <td class="cart_product_desc">
-                                            <a href="javascript:void(0)" class="btn-outline-primary border p-2">Detail Pesanan</a>
-                                            <a href="javascript:void(0)" class="btn-outline-primary border p-2">Update Status</a>
+                                            <a href="javascript:void(0)">Detail Pesanan</a>
+                                            <a href="javascript:void(0)">Update Status</a>
                                         </td>
                                     </tr>
                                 <?php } ?>
