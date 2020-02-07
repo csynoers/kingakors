0c37bcf33821c8770dfa8e9e950c7122ff4dca8a
csynoers <3s0c9m7@gmail.com>
2020-02-07 23:22:09 +0700

    detail pesanan >> content

--- application/views/menu_a/Vdetailpemesanan.php
+++ application/views/menu_a/Vdetailpemesanan.php
@@ -5,5 +5,35 @@
                 <h2>Detail Pemesanan</h2>
             </div>
         </div>
+        <div class="col-12 col-lg-12">
+            <div class="table-responsive">
+                <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>john@example.com</td>
+                            </tr>
+                            <tr>
+                                <td>Mary</td>
+                                <td>Moe</td>
+                                <td>mary@example.com</td>
+                            </tr>
+                            <tr>
+                                <td>July</td>
+                                <td>Dooley</td>
+                                <td>july@example.com</td>
+                            </tr>
+                        </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
